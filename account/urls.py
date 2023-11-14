95d424d54c24b478ed9e878b63f06fb662ab26e1
Suhail Khan <suhailkhan6267@gmail.com>
2023-11-14 15:24:44 +0530

    first commit

---
+++ account/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+
+urlpatterns = [
+    path('register/', views.UserRegistrationView.as_view(), name= 'register'),
+    path('login/', views.UserLoginView.as_view()),
+    path('profile/', views.UserProfileView.as_view(), name='profile'),
+    path('changepassword/', views.UserChangePasswordView.as_view(), name='changepassword'),
+    path('send-reset-password-email/', views.SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
+    path('reset-password/<uid>/<token>/', views.UserPasswordResetView.as_view(), name='reset-password'),
+   
+
+]
